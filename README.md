c535729e122413d21ba6db8536ee5b049d91dc3a
jamesrodda <james@mbp.home>
2017-05-07 21:02:33 +0100

    Add support for username/password authentication

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Energenie MiHome gateway plugin for [Homebridge](https://github.com/nfarina/home
 # Installation
 
 1. Install Homebridge using: `npm install -g homebridge`
-2. Install this plugin using: `npm install -g homebridge-mihomegatewat`
+2. Install this plugin using: `npm install -g homebridge-mihomegateway`
 3. Update your configuration file. See the sample below.
 
 # Updating
@@ -25,6 +25,7 @@ This will all be automated in upcoming releases but for now a bit of manual work
     "accessory": "MiHomeGateway",
     "name": "",
     "username": "",
+    "password": "",
     "api_key": "",
     "device_id": ""
 }]
@@ -32,6 +33,7 @@ This will all be automated in upcoming releases but for now a bit of manual work
 - `accessory` - must be set to **MiHomeGateway**
 - `name` - the name of the accessory to control
 - `username` - call the [register user service](https://mihome4u.co.uk/docs/api-documentation/users-api/sign-up-a-new-user) to create a new api user
+- `password` - either provide the password you used to register with or provide the api_key explicitly below. Your password is only used for one initial authentication call. Subsequent calls will use the api key obtained during authentication.
 - `api_key` - call the [user profile service](https://mihome4u.co.uk/docs/api-documentation/users-api/fetch-user-profile) using the credentials you provided previously to get an api key
 - `device_id` - call the [subdevices service](https://mihome4u.co.uk/docs/api-documentation/subdevices-api/list-all-subdevices) to get the device id for the device you wish to control
 
@@ -40,4 +42,3 @@ This will all be automated in upcoming releases but for now a bit of manual work
 - List all subdevices on gateway
 - Convert to a platform
 - Automate registration
-- Accept password for initial authentication or api key directly
--- index.js
+++ index.js
@@ -9,23 +9,67 @@ module.exports = function(homebridge) {
 }
 
 function MiHomeGatewayAccessory(log, config) {
+  if (!config) {
+    this.log("No configuration provided for MiHomeGateway. Disabling.");
+    this.disabled = true;
+    return;
+  }
+
   this.log = log;
+  this.log("begin constructor");
+
   this.name = config["name"];
   this.username = config["username"];
-  this.accessToken = config["api_key"];
   this.deviceId = config["device_id"];
   
+  this.baseUrl = "https://mihome4u.co.uk/api/v1/";
+
   this.service = new Service.Switch(this.name);
-  
+  this.authenticate(config, this.registerService);
+}
+
+MiHomeGatewayAccessory.prototype.registerService = function() {
   this.service
     .getCharacteristic(Characteristic.On)
     .on('set', this.setState.bind(this));
 }
+
+MiHomeGatewayAccessory.prototype.authenticate = function(config, callback) {
+  if (config["api_key"]) {
+    this.accessToken = config["api_key"];
+    callback.call(this);
+  }
+  else {
+    this.log("authenticating");
+    request.post({
+      url: this.baseUrl + "users/profile",
+      auth: {
+        user: this.username,
+        pass: config["password"],
+        sendImmediately: true
+      }
+    }, function(err, response, body) {
+      if (!err && response.statusCode == 200) {
+        var json = JSON.parse(body);
+        if (json.status == "success") {
+          this.log("authentication success");
+          this.accessToken = json.data.api_key;
+            callback.call(this);
+        }
+      }
+      else {
+        this.log("Error '%s' authenticating. Response: %s", err, body);
+        throw "Authentication failed. See log for details.";
+      }
+    }.bind(this));
+  }
+}
   
 MiHomeGatewayAccessory.prototype.setState = function(state, callback) {
-    var endpoint = state ? "power_on" : "power_off";
+  var endpoint = state ? "power_on" : "power_off";
+  this.log("start %s", endpoint);
   request.post({
-    url: "https://mihome4u.co.uk/api/v1/subdevices/" + endpoint,
+    url: this.baseUrl + "subdevices/" + endpoint,
     auth: {
         user: this.username,
         pass: this.accessToken,
@@ -35,7 +79,7 @@ MiHomeGatewayAccessory.prototype.setState = function(state, callback) {
   }, function(err, response, body) {
 
     if (!err && response.statusCode == 200) {
-      this.log("State change complete.");
+      this.log("%s complete", endpoint);
       
       callback(null); // success
     }
