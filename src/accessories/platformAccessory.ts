5b6002315ca84f42d228cd8dc72db2b7fb6e85dc
Kristian Matthews <kristian.matthews@me.com>
2020-05-19 15:11:01 +0100

    Refactor accessories
    * Set accessory information to `MiHomePlatformAccessory`.
    * Handle `SET` and `GET` requests from HomeKit to `MiHomePlatformAccessory`.
    * Update the state of a Characteristic asynchronously instead of using the `on('get')` handlers.

--- src/accessories/platformAccessory.ts
+++ src/accessories/platformAccessory.ts
@@ -1,5 +1,6 @@
-import type { PlatformAccessory } from 'homebridge';
+import type { PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
 
+import { MANUFACTURER, APP_MATCHING_IDENTIFIER } from '../settings';
 import { MiHomeGatewayPlatform } from '../platform';
 
 /**
@@ -13,7 +14,60 @@ export abstract class MiHomePlatformAccessory {
     protected readonly platform: MiHomeGatewayPlatform,
     protected readonly accessory: PlatformAccessory,
   ) {
-    // TODO
+
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, MANUFACTURER)
+      .setCharacteristic(this.platform.Characteristic.Model, this.accessory.context.device_type)
+      .setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.label)
+      .setCharacteristic(this.platform.Characteristic.AppMatchingIdentifier, APP_MATCHING_IDENTIFIER);
+  }
+
+  /**
+   * Handle "SET" requests from HomeKit
+   * These are sent when the user changes the state of an accessory, for example, turning on a Switch.
+   */
+  async setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    
+    const action = value ? 'power_on' : 'power_off';
+
+    try {
+      await this.platform.EnergenieApi.toggleSocketPower(this.accessory.context.device.id, action);
+
+      this.platform.log.debug('Set Characteristic On ->', value);
+
+      callback(null);
+    } catch (err) {
+      this.platform.log.error('Error setting Characteristic On ->', action, err);
+      callback(err);
+    }
+  }
+
+  /**
+   * Handle the "GET" requests from HomeKit
+   * These are sent when HomeKit wants to know the current state of the accessory.
+   * 
+   * The Energenie API doesn't actually return an accurate value for power_state.
+   * If the switch is toggled phsyically or via another route, for example; Alexa,
+   * the new value is not reported by the API.
+   * Investigations ongoing to find a solution.
+   */
+  async getOn(callback: CharacteristicGetCallback) {
+
+    // The Energenie API doesn't actually return an accurate value for power_state.
+    // If the switch is toggled phsyically or via another route, i.e. Alexa, the new
+    // value is not reported by the API. Investigations ongoing to find a solution.
+    try {
+      const device = await this.platform.EnergenieApi.getSubdeviceInfo(this.accessory.context.device.id);
+      const isOn = device.power_state === 1;
+
+      this.platform.log.debug('Get Characteristic On ->', isOn);
+
+      callback(null, isOn);
+    } catch (err) {
+      this.platform.log.error('Error getting Characteristic On ->', err);
+      callback(err);
+    }
   }
 
 }
